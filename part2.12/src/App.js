558e721b829a2183afcb888211f55bc2e58421d3
Wo Ming Boaz Cheung <92407344+rethegreat@users.noreply.github.com>
2022-09-27 12:05:40 -0400

    added basic 2.12

--- part2.12/src/App.js
+++ part2.12/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [filterText, setFilterText] = useState("");
+
+  const [countryData, setCountryData] = useState([]);
+
+  const [filteredCountryData, setFilteredCountryData] = useState([]);
+
+  const [countryList, setCountryList] = useState([]);
+
+
+  useEffect(() => {
+    console.log("effect");
+
+    axios
+      .get("http://restcountries.com/v3.1/all", { mode: "no-cors" })
+      .then((response) => {
+        console.log("promise fulfilled");
+
+        
+
+        setCountryData(response.data);
+        setFilteredCountryData(response.data);
+        
+        setCountryList(response.data.map(x => x.name.common))
+      });
+    
+    
+  }, []);
+
+  const handleFilterChange = (event) => {
+    setFilterText(event.target.value);
+    const filterLength = event.target.value.length;
+    const result = filteredCountryData.filter((countryData) => countryData.name.common.substring(0, filterLength).toUpperCase() === event.target.value.toUpperCase());
+    setFilteredCountryData(result)
+    setCountryList(countryList.filter((countryData) => countryData.substring(0, filterLength).toUpperCase() === event.target.value.toUpperCase()))
+
+    
+      
+  };
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4>
+        Find Countries{" "}
+        <input value={filterText} onChange={handleFilterChange} />
+      </h4>
+    
+      {countryList.length > 10 && <p>Too many matches, specify another filter</p>}
+      {countryList.length < 10 && countryList.map((country) => <p key={country}>{country}</p>)}
     </div>
   );
 }
